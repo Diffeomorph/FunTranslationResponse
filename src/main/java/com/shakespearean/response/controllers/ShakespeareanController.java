74f4e2ab1f709d1d3731e7f3873e402f1cd24150
Diffeomorph <64492048+Diffeomorph@users.noreply.github.com>
2022-04-15 22:48:58 +0100

    cleaned up unused lines

--- src/main/java/com/shakespearean/response/controllers/ShakespeareanController.java
+++ src/main/java/com/shakespearean/response/controllers/ShakespeareanController.java
@@ -1,19 +1,13 @@
 package com.shakespearean.response.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shakespearean.response.services.PokemonRequest;
 import com.shakespearean.response.services.PokemonResponse;
 import com.shakespearean.response.services.ShakespeareRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 /**
  * The ShakespeareanController, given a pokemon name, returns a "shakespearean" response.
@@ -21,16 +15,9 @@ import java.nio.charset.StandardCharsets;
 @RestController
 public class ShakespeareanController {
 
-
     @Autowired
     private PokemonRequest pokemonRequest;
     private ShakespeareRequest shakespeareRequest;
-
-    // Get all pokemon responses
-    @GetMapping("/pokemon")
-    void getAllPokemon() {
-        return;
-    }
 
     // Get particular pokemon response
     @GetMapping("/pokemon/{name}")
@@ -49,11 +36,10 @@ public class ShakespeareanController {
         }
     }
 
-    // Get particular pokemon response
+    // Get particular pokemon with translated response
     @GetMapping("/pokemon/translated/{name}")
     String getTranslatedPokemon(@PathVariable String name) throws JsonProcessingException {
         String[] res = pokemonRequest.getPokemonbyName(name);
-
         String translation = shakespeareRequest.getShakespeareanTranslation(res[1]);
 
         PokemonResponse pokemonResponse = new PokemonResponse(res[0], translation, res[2], res[3]);
@@ -68,21 +54,5 @@ public class ShakespeareanController {
         }
     }
 
-    /*
-    @PostMapping("/pokemon")
-    void submitPokemon(@RequestBody pokemon){
-        return;
-    }
-    */
-
-    @DeleteMapping("/pokemon/{id}")
-    void deletePokemon(){
-        // code
-    }
-
-    @PutMapping("/pokemon/{id}")
-    void updatePokemon(){
-        //code
-    }
 
 }
