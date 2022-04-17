0f8ebd7da39e90c76337fe457de8171dcc08ac16
Diffeomorph <64492048+Diffeomorph@users.noreply.github.com>
2022-04-17 11:43:10 +0100

    fixed autowiring in controller

--- src/main/java/com/shakespearean/response/controllers/ShakespeareanController.java
+++ src/main/java/com/shakespearean/response/controllers/ShakespeareanController.java
@@ -17,6 +17,8 @@ public class ShakespeareanController {
 
     @Autowired
     PokemonRequest pokemonRequest;
+
+    @Autowired
     ShakespeareRequest shakespeareRequest;
 
     // Get particular pokemon response
